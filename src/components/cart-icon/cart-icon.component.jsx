f04a0549f53a074324c5285d1c90d9c1ed9ec465
Fernando-Melo <fmelo@icapitalnetwork.com>
2021-10-24 19:09:07 +0100

    132 reselect

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { connect } from 'react-redux';
 
 import { toggleCartHidden } from '../../redux/cart/cart.actions'
+import { selectCartItemsCount } from '../../redux/cart/cart.selectors';
+
 import {ReactComponent as ShoppingIcon} from '../../assets/shopping-bag.svg';
 
 import './cart-icon.styles.scss';
@@ -17,4 +19,8 @@ const mapDispatchToProps = dispatch => ({
   toggleCartHidden: () => dispatch(toggleCartHidden())
 })
 
-export default connect(null,mapDispatchToProps)(CartIcon);
+const mapStateToProps = state => ({
+  itemsCount: selectCartItemsCount(state)
+});
+
+export default connect(mapStateToProps,mapDispatchToProps)(CartIcon);
